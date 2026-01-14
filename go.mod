b5f98528e7698dab8ab502e2e8890eff59a8d88e
akshay <akshayjha2111@gmail.com>
2026-01-14 10:33:24 +0530

    adding pubsub

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/akshayjha21/RescueMesh.git
 
 go 1.25.4
 
-require github.com/libp2p/go-libp2p v0.46.0
+require (
+	github.com/libp2p/go-libp2p v0.46.0
+	github.com/libp2p/go-libp2p-pubsub v0.15.0
+)
 
 require (
 	github.com/benbjohnson/clock v1.3.5 // indirect
@@ -11,8 +14,10 @@ require (
 	github.com/davidlazar/go-crypto v0.0.0-20200604182044-b73af7476f6c // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
 	github.com/flynn/noise v1.1.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/huin/goupnp v1.3.0 // indirect
 	github.com/ipfs/go-cid v0.5.0 // indirect
 	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
--- internal/p2p/pubsub.go
+++ internal/p2p/pubsub.go
@@ -1,1 +1,103 @@
 package p2p
+
+import (
+	"context"
+	"encoding/json"
+
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/peer"
+)
+
+// ChatRoom represents a subscription to a single PubSub topic. Messages
+// can be published to the topic with ChatRoom.Publish, and received
+// messages are pushed to the Messages channel.
+type ChatRoom struct {
+	// Messages is a channel of messages received from other peers in the chat room
+	Messages chan *ChatMessage
+	ctx      context.Context
+	ps       *pubsub.PubSub
+	topic    *pubsub.Topic
+	sub      *pubsub.Subscription
+
+	roomName string
+	self     peer.ID
+	nick     string
+}
+
+type ChatMessage struct {
+	Message    string
+	SenderID   string
+	SenderNick string
+}
+
+//chatroom buff sixe
+//func topicName
+//joinChat
+
+const ChatRoomBufSize = 128
+
+func topicName(roomName string) string {
+	return "chat-Room" + roomName
+}
+
+func JoinChat(ctx context.Context, ps *pubsub.PubSub, selfId peer.ID, nickname string, roomName string) (*ChatRoom, error) {
+	//join the pubsub topic
+	topic, err := ps.Join(topicName(roomName))
+	if err != nil {
+		return nil, err
+	}
+	//and subscribing to it
+	sub, err := topic.Subscribe()
+	if err != nil {
+		return nil, err
+	}
+
+	cr := &ChatRoom{
+		ctx:      ctx,
+		ps:       ps,
+		topic:    topic,
+		sub:      sub,
+		self:     selfId,
+		nick:     nickname,
+		roomName: roomName,
+		Messages: make(chan *ChatMessage, ChatRoomBufSize),
+	}
+
+	// start reading messages from the subscription in a loop
+	go cr.readLoop()
+	return cr, nil
+
+}
+func (cr *ChatRoom) Publish(message string) error {
+	m := ChatMessage{
+		Message:    message,
+		SenderID:   cr.self.String(),
+		SenderNick: cr.nick,
+	}
+	msgBytes, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	return cr.topic.Publish(cr.ctx, msgBytes)
+}
+
+func (cr *ChatRoom) readLoop() {
+	for {
+		msg, err := cr.sub.Next(cr.ctx)
+		if err != nil {
+			close(cr.Messages)
+			return
+		}
+		// only forward messages delivered by others
+		if msg.ReceivedFrom == cr.self {
+			continue
+		}
+		cm := new(ChatMessage)
+		err = json.Unmarshal(msg.Data, cm)
+		if err != nil {
+			continue
+		}
+		// send valid messages onto the Messages channel
+		cr.Messages <- cm
+	}
+}
